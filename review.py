180097c0f79c506748d66cd99a035ff7e0daff65
April Tseng <aprilmow1018129@gmail.com>
2019-03-11 19:05:51 +0800

    another filter

--- review.py
+++ review.py
@@ -20,3 +20,9 @@ for d in data: #每一筆留言
 		new.append(d) #加到new這個集合
 print('there are', len(new), 'reviews which lenth are shorter than 100')
 print(new[0])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('there are', len(good), 'reviews that mention good')
