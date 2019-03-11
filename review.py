9dd97bb4dbb90000c4f757c3dcdca842c16228f7
April Tseng <aprilmow1018129@gmail.com>
2019-03-11 18:59:57 +0800

    Add filtering option

--- review.py
+++ review.py
@@ -9,7 +9,14 @@ with open('reviews.txt', 'r') as f:
 
 print('reading completed, total', len(data), 'data')
 
-sum_len = 0
+sum_len = 0 #sum一開始為0
 for d in data:
-	sum_len = sum_len + len(d)
+	sum_len = sum_len + len(d) #使sum累計
 print('average is', sum_len / len(data))
+
+new = [] #create new
+for d in data: #每一筆留言
+	if len(d) < 100:
+		new.append(d) #加到new這個集合
+print('there are', len(new), 'reviews which lenth are shorter than 100')
+print(new[0])
